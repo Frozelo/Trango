0e1030f27a663fd62a37a56f4e66de9401711df8
ivansizov <sizovivan.05@gmail.com>
2025-01-06 01:16:16 +0700

    cobra test usage

--- main.go
+++ main.go
@@ -1,5 +1,20 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"log"
 
+	"github.com/spf13/cobra"
+)
+
+func main() {
+	testCmd := cobra.Command{
+		Use: "test",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello")
+		},
+	}
+	if err := testCmd.Execute(); err != nil {
+		log.Fatal(err)
+	}
 }
